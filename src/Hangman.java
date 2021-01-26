9a80ecc9b0a7743e8ce49df4fd469c0dddbe0421
PTSkAlman <joel.abrahamsson@elev.ga.ntig.se>
2021-01-26 11:14:26 +0100

    Försökte skriva om gissningsmetoden eftersom att den var väldigt lång och svår att läsa och den fungerade inte heller

--- src/Hangman.java
+++ src/Hangman.java
@@ -6,24 +6,66 @@ import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
-        String word = pickRandomWord();
-        System.out.println(letterCount());
         startGuessing();
     }
 
     private static void startGuessing() {
+        System.out.println(letterCount());
+        Scanner input = new Scanner(System.in);
+        String guess = input.nextLine();
+        int tries = 10;
+        String currentWord = null;
+        boolean isRunning = true;
+        while (isRunning) {
+            if (tries == 0) {
+                System.out.println("Du förlorade!");
+                isRunning = false;
+            } else if (currentWord.equals(pickRandomWord())) {
+                System.out.println("Du vann!");
+                isRunning = false;
+            }
+
+            if (pickRandomWord().contains(guess)) {
+                if (guess.length() < 1) {
+                    if (/*guess.charAt(0) == charOf(pickRandomWord())*/ false) {
+
+                    } else {
+                        --tries;
+                        System.out.println("Du har " + tries + " kvar");
+                    }
+                }
+            }
+        }
+    }
+/*
+    private static void startGuessing() {
         Scanner guess = new Scanner(System.in);
         int tries = 10;
         String wrongGuess = "";
-        String myGuess = "";
-        int myGuessPos = pickRandomWord().indexOf(myGuess);
-        while (tries > 0) {
+        String myGuess;
+        StringBuilder currentWord = new StringBuilder(String.valueOf(letterCount()));
+        int myGuessPos;
+        while (tries > 0 || currentWord.equals(pickRandomWord())) {
             myGuess = guess.nextLine();
-            if (pickRandomWord().indexOf(myGuess) >= 0) {
-                while (myGuessPos >= 0) {
-                    System.out.println(myGuessPos);
+            myGuessPos = pickRandomWord().indexOf(myGuess);
+            if (pickRandomWord().contains(myGuess)) {
+
+                while (myGuessPos >= 0 && myGuessPos < myGuess.length()) {
+                    currentWord.setCharAt(myGuessPos, myGuess.charAt(0));
                     myGuessPos = pickRandomWord().indexOf(myGuess, ++myGuessPos);
+                    System.out.println(currentWord);
                 }
+
+                for (int i = 0 ; i<pickRandomWord().length() ; i++) {
+                    if (myGuess.equals(pickRandomWord())) {
+                        currentWord.replace(0, pickRandomWord().length(), pickRandomWord());
+                        System.out.println(currentWord);
+                        i = pickRandomWord().length();
+                    } else if (myGuess.substring(0,1).equals(pickRandomWord().charAt(i))) {
+                        currentWord.setCharAt(i, myGuess.charAt(0));
+                    }
+                }
+
             } else {
                 wrongGuess += myGuess;
                 --tries;
@@ -34,6 +76,7 @@ public class Hangman {
             }
         }
     }
+ */
 
     private static char[] letterCount() {
         int letterAmount = pickRandomWord().length();
